45b999e600b57324063104fb758c4843c0d36a88
Jarosław Mądry <jarasssss@gmail.com>
2018-04-18 16:40:21 +0200

    Simple hsv laser detection

--- LaserScan/main.cpp
+++ LaserScan/main.cpp
@@ -1,7 +1,10 @@
 #include <opencv2/opencv.hpp>
 #include <Eigen/Core>
+#include <Eigen/Dense>
 using namespace cv;
 using namespace std;
+
+
 
 /// Matrices to store images
 
@@ -21,7 +24,7 @@ int hsv_h_min = 166;
 const int hsv_s_min_max = 255;
 int hsv_s_min = 37;
 const int hsv_v_min_max = 255;
-int hsv_v_min = 180;
+int hsv_v_min = 255;
 
 const int hsv_h_max_max = 255;
 int hsv_h_max = 255;
@@ -30,29 +33,13 @@ int hsv_s_max = 255;
 const int hsv_v_max_max = 255;
 int hsv_v_max = 255;
 
-void find_first(Mat& input, Mat& output)
-{
-	int rows = input.rows;
-	int columns = input.cols;
-	Mat model = input;
-	int flaga = 0;
-	for (int i = 0; i < rows; i++)
-	{
-		flaga = 0;
-		for (int j = 0; j < columns; j++)
-		{
-			if (model.at<uchar>(i, j) > 0&& flaga==0)
-			{
-				flaga = 1;
-				output.at<uchar>(i, j) = 255;
-			}
-			else
-			{
-				output.at<uchar>(i, j) = 0;
-			}
-		}
-	}
-}
+bool find_laser(Mat frame, vectorVector2d &TestVect2d);
+bool calibrate_laser(Mat frame, Mat cameraMatrix, Mat distCoeffs, Eigen::Vector4d &PlaneAtCheckerboard);
+bool projectImagePointsOntoPlane(const vectorVector2d &pts,
+	vectorVector3d &pts3d,
+	const cv::Mat &cameraMatrix,
+	const Eigen::Vector4d &planeEq);
+
 
 int main(int, char**)
 {
@@ -72,111 +59,32 @@ int main(int, char**)
 
 	FileStorage fs2("cam_param.yml", FileStorage::READ);
 
-	Mat cameraMatrix2, distCoeffs2;
-	fs2["cameraMatrix"] >> cameraMatrix2;
-	fs2["distCoeffs"] >> distCoeffs2;
+	Mat cameraMatrix(3, 3, CV_64F), distCoeffs(3, 3, CV_64F);
+	fs2["cameraMatrix"] >> cameraMatrix;
+	fs2["distCoeffs"] >> distCoeffs;
 
+	//cout << cameraMatrix2 << endl;
+	//cout << cameraMatrix2.at<float>(0, 0);
 	fs2.release();
 
 
 	while (1) {
 		Mat frame;
+		vectorVector2d laserpoints2d;
+		Eigen::Vector4d PlaneAtCheckerboard;
 		if (!cap.read(frame))
 		cap >> frame;
-		//undistort(tmp, frame, cameraMatrix2, distCoeffs2);
 		imshow("Film", frame);
 
-
-		Size patternsize(7, 6);
-		vector<Point2f> corners;
-
-		bool patternfound = findChessboardCorners(frame, patternsize, corners,
-			CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
-			+ CALIB_CB_FAST_CHECK);
-		if (patternfound)
-		{
-			Mat subpixel_BW;
-			cvtColor(frame, subpixel_BW, CV_BGR2GRAY);
-			cornerSubPix(subpixel_BW, corners, Size(11, 11), Size(-1, -1),
-				TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
-			//if (i == 0)
-			//cout << corners;
-			//wek.push_back(corners);
-			//dobra_wek.push_back(dobra);
-
-			//namedWindow("Chessboard");
-			drawChessboardCorners(frame, patternsize, Mat(corners), patternfound);
-			//imshow("Chessboard", frame);
 		
-			
-			vector<Point3f> axis;
-			axis.push_back(cv::Point3d(0, 0, 0));
-			axis.push_back(cv::Point3d(1, 0, 0));
-			axis.push_back(cv::Point3d(0, 1, 0));
-			axis.push_back(cv::Point3d(0, 0, -1));
+		if (calibrate_laser(frame, cameraMatrix, distCoeffs, PlaneAtCheckerboard)
+			&& find_laser(frame, laserpoints2d))
+		{
 
-			vector<Point3f> objp;
-			for (int y = patternsize.height -1; y >= 0 ; y--)
-			{
-				for (int x = patternsize.width-1; x >= 0; x--)
-				{
-					objp.push_back(cv::Point3d(x, y, 0));
-				}
-			}
-			Mat rvec(3, 1, DataType<double>::type);
-			Mat tvec(3, 1, DataType<double>::type);
-			//vector<Point3f> rvec;
-			//vector<Point3f> tvec;
-			vector<Point2f> imgpts;
-			//# Find the rotation and translation vectors.
-			solvePnP(objp, corners, cameraMatrix2, distCoeffs2, rvec, tvec);
+			vectorVector3d laserpoints3d;
 
-			Mat rotMat;
-			Rodrigues(rvec, rotMat);
-
-			Mat Transf(4,4,CV_64F);
-			Transf.at<double>(0, 0) = rotMat.at<double>(0, 0);
-			Transf.at<double>(1, 0) = rotMat.at<double>(1, 0);
-			Transf.at<double>(2, 0) = rotMat.at<double>(2, 0);
-			Transf.at<double>(0, 1) = rotMat.at<double>(0, 1);
-			Transf.at<double>(1, 1) = rotMat.at<double>(1, 1);
-			Transf.at<double>(2, 1) = rotMat.at<double>(2, 1);
-			Transf.at<double>(0, 2) = rotMat.at<double>(0, 2);
-			Transf.at<double>(1, 2) = rotMat.at<double>(1, 2);
-			Transf.at<double>(2, 2) = rotMat.at<double>(2, 2);
-
-			Transf.at<double>(0, 3) = tvec.at<double>(0);
-			Transf.at<double>(1, 3) = tvec.at<double>(1);
-			Transf.at<double>(2, 3) = tvec.at<double>(2);
-
-			Transf.at<double>(3, 0) = 0;
-			Transf.at<double>(3, 1) = 0;
-			Transf.at<double>(3, 2) = 0;
-			Transf.at<double>(3, 3) = 1;
-
-			cout<<Transf<<endl;
-
-
-			Eigen::Vector4d PlaneAtCamera;
-			PlaneAtCamera << 0, 0, 1, 0;
-
-			//ret, rvecs, tvecs, inliers = cv2.solvePnP(objp, corners2, mtx, dist)
-			//# project 3D points to image plane
-			projectPoints(axis, rvec, tvec, cameraMatrix2, distCoeffs2, imgpts);
-			//imgpts, jac = cv2.projectPoints(axis, rvec, tvecs, mtx, dist)
-			//img = draw(img, corners2, imgpts)
-
-			int myradius = 5;
-			//for (int i = 0; i<imgpts.size(); i++)
-
-				circle(frame, cvPoint(imgpts[0].x, imgpts[0].y), myradius, CV_RGB(255, 0, 255), -1, 8, 0);
-				circle(frame, cvPoint(imgpts[1].x, imgpts[1].y), myradius, CV_RGB(255, 0, 0), -1, 8, 0);
-				line(frame, cvPoint(imgpts[0].x, imgpts[0].y), cvPoint(imgpts[1].x, imgpts[1].y), CV_RGB(255, 0, 0),4);
-				circle(frame, cvPoint(imgpts[2].x, imgpts[2].y), myradius, CV_RGB(0, 255, 0), -1, 8, 0);
-				line(frame, cvPoint(imgpts[0].x, imgpts[0].y), cvPoint(imgpts[2].x, imgpts[2].y), CV_RGB(0, 255, 0),4);
-				circle(frame, cvPoint(imgpts[3].x, imgpts[3].y), myradius, CV_RGB(0, 0, 255), -1, 8, 0);
-				line(frame, cvPoint(imgpts[0].x, imgpts[0].y), cvPoint(imgpts[3].x, imgpts[3].y), CV_RGB(0, 0, 255),4);
-			imshow("Axis", frame);
+			projectImagePointsOntoPlane(laserpoints2d, laserpoints3d, cameraMatrix, PlaneAtCheckerboard);
+			cout << "calculeted:" << laserpoints3d.at(0) << endl;
 		}
 		
 
@@ -212,6 +120,123 @@ int main(int, char**)
 	return 0;
 }
 
+bool find_laser(Mat frame, vectorVector2d &laserpoints2d)
+{
+
+	Mat laser;
+	inRange(frame, Scalar(hsv_h_min, hsv_s_min, hsv_v_min), Scalar(hsv_h_max, hsv_s_max, hsv_v_max), laser);
+	imshow("laser", laser);
+	bool flaga = false;
+	for (int i = 0; i < frame.rows; ++i)
+	{
+		for (int j = 0; j < frame.cols; ++j)
+		{
+			if (laser.at<uchar>(i, j) > 0)
+			{
+				Eigen::Vector2d punkty2d(j, i);
+				laserpoints2d.push_back(punkty2d);
+				flaga = true;
+			}
+		}
+	}
+	return flaga;
+}
+bool calibrate_laser(Mat frame, Mat cameraMatrix, Mat distCoeffs, Eigen::Vector4d &PlaneAtCheckerboard)
+{
+	Size patternsize(7, 6);
+	vector<Point2f> corners;
+
+	bool patternfound = findChessboardCorners(frame, patternsize, corners,
+		CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
+		+ CALIB_CB_FAST_CHECK);
+	if (patternfound)
+	{
+		Mat subpixel_BW;
+		cvtColor(frame, subpixel_BW, CV_BGR2GRAY);
+		cornerSubPix(subpixel_BW, corners, Size(11, 11), Size(-1, -1),
+			TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+		//if (i == 0)
+		//cout << corners;
+		//wek.push_back(corners);
+		//dobra_wek.push_back(dobra);
+
+		//namedWindow("Chessboard");
+		drawChessboardCorners(frame, patternsize, Mat(corners), patternfound);
+		//imshow("Chessboard", frame);
+
+
+		vector<Point3f> axis;
+		axis.push_back(cv::Point3d(0, 0, 0));
+		axis.push_back(cv::Point3d(1, 0, 0));
+		axis.push_back(cv::Point3d(0, 1, 0));
+		axis.push_back(cv::Point3d(0, 0, -1));
+
+		vector<Point3f> objp;
+		for (int y = patternsize.height - 1; y >= 0; y--)
+		{
+			for (int x = patternsize.width - 1; x >= 0; x--)
+			{
+				objp.push_back(cv::Point3d(x, y, 0));
+			}
+		}
+		Mat rvec(3, 1, DataType<double>::type);
+		Mat tvec(3, 1, DataType<double>::type);
+		//vector<Point3f> rvec;
+		//vector<Point3f> tvec;
+		vector<Point2f> imgpts;
+		//# Find the rotation and translation vectors.
+		solvePnP(objp, corners, cameraMatrix, distCoeffs, rvec, tvec);
+
+		//cout << objp.at(0) << endl;
+		cout << "tvec:"<<tvec << endl;
+
+		Mat rotMat;
+		Rodrigues(rvec, rotMat);
+
+		Eigen::Matrix<double, 4, 4> Transf;
+		Transf << rotMat.at<double>(0, 0), rotMat.at<double>(0, 1), rotMat.at<double>(0, 2), tvec.at<double>(0),
+			rotMat.at<double>(1, 0), rotMat.at<double>(1, 1), rotMat.at<double>(1, 2), tvec.at<double>(1),
+			rotMat.at<double>(2, 0), rotMat.at<double>(2, 1), rotMat.at<double>(2, 2), tvec.at<double>(2),
+			0, 0, 0, 1;
+
+
+		Eigen::Vector4d PlaneAtCamera;
+		PlaneAtCamera << 0, 0, 1, 0;
+
+		
+		PlaneAtCheckerboard = Transf.inverse().transpose()*PlaneAtCamera;
+
+		//ret, rvecs, tvecs, inliers = cv2.solvePnP(objp, corners2, mtx, dist)
+		//# project 3D points to image plane
+		projectPoints(axis, rvec, tvec, cameraMatrix, distCoeffs, imgpts);
+
+		int myradius = 5;
+
+		circle(frame, cvPoint(imgpts[0].x, imgpts[0].y), myradius, CV_RGB(255, 0, 255), -1, 8, 0);
+		circle(frame, cvPoint(imgpts[1].x, imgpts[1].y), myradius, CV_RGB(255, 0, 0), -1, 8, 0);
+		line(frame, cvPoint(imgpts[0].x, imgpts[0].y), cvPoint(imgpts[1].x, imgpts[1].y), CV_RGB(255, 0, 0), 4);
+		circle(frame, cvPoint(imgpts[2].x, imgpts[2].y), myradius, CV_RGB(0, 255, 0), -1, 8, 0);
+		line(frame, cvPoint(imgpts[0].x, imgpts[0].y), cvPoint(imgpts[2].x, imgpts[2].y), CV_RGB(0, 255, 0), 4);
+		circle(frame, cvPoint(imgpts[3].x, imgpts[3].y), myradius, CV_RGB(0, 0, 255), -1, 8, 0);
+		line(frame, cvPoint(imgpts[0].x, imgpts[0].y), cvPoint(imgpts[3].x, imgpts[3].y), CV_RGB(0, 0, 255), 4);
+		imshow("Axis", frame);
+
+		/*vectorVector2d laserpoints2d;
+		vectorVector3d laserpoints3d;
+		Eigen::Vector2d punkty2d(corners.at(7*6-1).x, corners.at(7 * 6 - 1).y);
+		laserpoints2d.push_back(punkty2d);
+
+
+		projectImagePointsOntoPlane(laserpoints2d, laserpoints3d, cameraMatrix, PlaneAtCheckerboard);
+		cout << "calculeted:" << laserpoints3d.at(0) << endl;*/
+
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 bool projectImagePointsOntoPlane(const vectorVector2d &pts,
 	vectorVector3d &pts3d,
 	const cv::Mat &cameraMatrix,
@@ -219,10 +244,10 @@ bool projectImagePointsOntoPlane(const vectorVector2d &pts,
 {
 	static constexpr double eps = 1e-6;
 
-	float fx = cameraMatrix.at<float>(0, 0);
-	float fy = cameraMatrix.at<float>(1, 1);
-	float cx = cameraMatrix.at<float>(0, 2);
-	float cy = cameraMatrix.at<float>(1, 2);
+	float fx = cameraMatrix.at<double>(0, 0);
+	float fy = cameraMatrix.at<double>(1, 1);
+	float cx = cameraMatrix.at<double>(0, 2);
+	float cy = cameraMatrix.at<double>(1, 2);
 
 	// camera center in homogeneous cooridinates
 	Eigen::Vector4d C;
