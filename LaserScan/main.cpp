9a602b09660107f17d6df1f62a7940a12d79c01a
Jarosław Mądry <jarasssss@gmail.com>
2018-05-10 12:03:54 +0200

    Added camera calibration.

--- LaserScan/main.cpp
+++ LaserScan/main.cpp
@@ -4,33 +4,25 @@
 using namespace cv;
 using namespace std;
 
-
-
-/// Matrices to store images
-
-Mat frame;
-
-
-/// Global Variables
 typedef std::vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d> > vectorVector2d;
 typedef std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > vectorVector3d;
 typedef std::vector<Eigen::Vector4d, Eigen::aligned_allocator<Eigen::Vector4d> > vectorVector4d;
 
-const int alpha_slider_max = 255;
-int alpha_slider = 50;
+/// Global Variables
 
 const int hsv_h_min_max = 255;
-int hsv_h_min = 166;
 const int hsv_s_min_max = 255;
-int hsv_s_min = 37;
 const int hsv_v_min_max = 255;
+const int hsv_h_max_max = 255;
+const int hsv_s_max_max = 255;
+const int hsv_v_max_max = 255;
+
+int hsv_h_min = 166;
+int hsv_s_min = 37;
 int hsv_v_min = 255;
 
-const int hsv_h_max_max = 255;
 int hsv_h_max = 255;
-const int hsv_s_max_max = 255;
 int hsv_s_max = 255;
-const int hsv_v_max_max = 255;
 int hsv_v_max = 255;
 
 
@@ -41,6 +33,7 @@ bool projectImagePointsOntoPlane(const vectorVector2d &pts,
 	vectorVector3d &pts3d,
 	const cv::Mat &cameraMatrix,
 	const Eigen::Vector4d &planeEq);
+bool camCalib();
 
 
 int main(int, char**)
@@ -50,15 +43,14 @@ int main(int, char**)
 	if (!cap.isOpened()) // check if we succeeded
 		return -1;
 
-	namedWindow("Film");
+	namedWindow("frame");
 
-
-	createTrackbar("hsv_h_min", "Film", &hsv_h_min, hsv_h_min_max);
+	/*createTrackbar("hsv_h_min", "Film", &hsv_h_min, hsv_h_min_max);
 	createTrackbar("hsv_s_min", "Film", &hsv_s_min, hsv_s_min_max);
 	createTrackbar("hsv_v_min", "Film", &hsv_v_min, hsv_v_min_max);
 	createTrackbar("hsv_h_max", "Film", &hsv_h_max, hsv_h_max_max);
 	createTrackbar("hsv_s_max", "Film", &hsv_s_max, hsv_s_max_max);
-	createTrackbar("hsv_v_max", "Film", &hsv_v_max, hsv_v_max_max);
+	createTrackbar("hsv_v_max", "Film", &hsv_v_max, hsv_v_max_max);*/
 
 	FileStorage fs2("cam_param.yml", FileStorage::READ);
 
@@ -73,25 +65,20 @@ int main(int, char**)
 	FileStorage fs_Plane("PlaneEq.yml", FileStorage::READ);
 	if (fs_Plane.isOpened())
 	{
-		//Mat cameraMatrix(3, 3, CV_64F), distCoeffs(3, 3, CV_64F);
 		fs_Plane["PlaneNx"] >> PlaneEquation(0);
 		fs_Plane["PlaneNy"] >> PlaneEquation(1);
 		fs_Plane["PlaneNz"] >> PlaneEquation(2);
 		fs_Plane["PlaneD"] >> PlaneEquation(3);
 
 		cout << PlaneEquation << endl;
-		//cout << cameraMatrix2.at<float>(0, 0);
 		fs_Plane.release();
 		waitKey(0);
 	}
 	else
 	{
-		
 		Eigen::MatrixXd cloud_of_points = Eigen::MatrixXd::Zero(3, 1);
 
-
 		while (1) {
-
 			vectorVector2d laserpoints2d;
 			Eigen::Vector4d PlaneAtCheckerboard;
 			Mat frame;
@@ -99,7 +86,7 @@ int main(int, char**)
 			{
 				if (!cap.read(frame))
 					cap >> frame;
-				imshow("Film", frame);
+				imshow("frame", frame);
 				Mat tresholded;
 				inRange(frame, Scalar(hsv_h_min, hsv_s_min, hsv_v_min), Scalar(hsv_h_max, hsv_s_max, hsv_v_max), tresholded);
 				imshow("tresholded", tresholded);
@@ -125,7 +112,6 @@ int main(int, char**)
 				projectImagePointsOntoPlane(laserpoints2d, laserpoints3d, cameraMatrix, PlaneAtCheckerboard);
 				//cout << "calculeted:" << laserpoints3d.at(0) << endl;
 
-				//Eigen::Vector3d test;
 				for (int i = 0; i < laserpoints3d.size(); i++)
 				{
 					cloud_of_points.conservativeResize(cloud_of_points.rows(), cloud_of_points.cols() + 1);
@@ -133,37 +119,6 @@ int main(int, char**)
 				}
 				PlaneEquation = best_plane_from_points(cloud_of_points);
 			}
-
-
-			//k = cv2.waitKey(0) & 0xFF
-			//if k == ord('s') :
-			//		cv2.imwrite(fname[:6] + '.png', img)
-
-
-			/*Mat hsv;
-			cvtColor(frame, hsv, COLOR_BGR2HSV);
-
-			Mat mask;
-
-			inRange(hsv, Scalar(hsv_h_min, hsv_s_min, hsv_v_min), Scalar(hsv_h_max, hsv_s_max, hsv_v_max), mask);
-
-			Mat frame_dilate;
-			int erosion_size = 2;
-			Mat element = getStructuringElement(MORPH_RECT, Size(2 * erosion_size + 1, 2 * erosion_size + 1), Point(erosion_size, erosion_size));
-			//cvtColor(mediana, mediana, COLOR_HSV2BGR);
-			dilate(mask, mask, element);
-
-			imshow("Film_mask", mask);
-
-			Mat linia;
-			mask.copyTo(linia);
-			find_first(mask, linia);
-			rotate(linia, linia, ROTATE_90_CLOCKWISE);
-			imshow("Film_linia", linia);*/
-			/*if (cv::waitKey(10)=='s')
-			{
-				break;
-			}*/
 		}
 	}
 
@@ -177,16 +132,10 @@ Eigen::Vector4d best_plane_from_points(Eigen::MatrixXd cloud_of_points)
 
 	Eigen::MatrixXd cloud_of_points_tmp;
 	cloud_of_points_tmp = cloud_of_points.replicate(1, 1);
-	// copy coordinates to  matrix in Eigen format
-	//size_t num_atoms = pts3d.size();
-	//Eigen::Matrix< Vector3::Scalar, Eigen::Dynamic, Eigen::Dynamic > coord(3, num_atoms);
-	//for (size_t i = 0; i < num_atoms; ++i) coord.col(i) = pts3d[i];
 
 	// calculate centroid
 	Eigen::Vector3d centroid(cloud_of_points.row(0).mean(), cloud_of_points.row(1).mean(), cloud_of_points.row(2).mean());
 
-	//cout <<"centroid"<< centroid << endl;
-	//cout <<"cloud"<< cloud_of_points.col(1) << endl << endl;
 
 	// subtract centroid
 	for (int i = 0; i < cloud_of_points.cols(); i++)
@@ -194,22 +143,18 @@ Eigen::Vector4d best_plane_from_points(Eigen::MatrixXd cloud_of_points)
 		cloud_of_points_tmp.block<3, 1>(0, i) -= centroid;
 	}
 
-	//cout << "tm" << cloud_of_points_tmp.col(1) << endl << endl;
-	// we only npeed the left-singular matrix here
-	
 	Eigen::Vector3d plane_normal;
 
-	auto svd = cloud_of_points_tmp.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV);
+	auto svd = cloud_of_points_tmp.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV);		//je¿eli nie dzia³a to tu mo¿e byæ Ÿle
 	plane_normal = svd.matrixU().rightCols<1>();
 	//cout << plane_normal << endl << endl;
 	
 	auto distance = centroid.transpose()*plane_normal;
 	Eigen::Vector4d PlaneEq;
-	PlaneEq.head<3>() = plane_normal;//, distance.value();
-	PlaneEq(3) = distance.value();
+	PlaneEq.head<3>() = plane_normal;
+	PlaneEq(3) = -distance.value();
 	//cout << distance << endl << endl;
 	cout << PlaneEq << endl << endl;
-	//return std::make_pair(centroid, plane_normal);
 	return PlaneEq;
 }
 
@@ -258,7 +203,6 @@ bool calculateCheckerboardVector(Mat frame, Mat cameraMatrix, Mat distCoeffs, Ei
 		drawChessboardCorners(frame, patternsize, Mat(corners), patternfound);
 		//imshow("Chessboard", frame);
 
-
 		vector<Point3f> axis;
 		axis.push_back(cv::Point3d(0, 0, 0));
 		axis.push_back(cv::Point3d(1, 0, 0));
@@ -290,14 +234,11 @@ bool calculateCheckerboardVector(Mat frame, Mat cameraMatrix, Mat distCoeffs, Ei
 			rotMat.at<double>(2, 0), rotMat.at<double>(2, 1), rotMat.at<double>(2, 2), tvec.at<double>(2),
 			0, 0, 0, 1;
 
-
 		Eigen::Vector4d PlaneAtCamera;
 		PlaneAtCamera << 0, 0, 1, 0;
 
-		
 		PlaneAtCheckerboard = Transf.inverse().transpose()*PlaneAtCamera;
 
-		//ret, rvecs, tvecs, inliers = cv2.solvePnP(objp, corners2, mtx, dist)
 		//# project 3D points to image plane
 		projectPoints(axis, rvec, tvec, cameraMatrix, distCoeffs, imgpts);
 
@@ -311,15 +252,6 @@ bool calculateCheckerboardVector(Mat frame, Mat cameraMatrix, Mat distCoeffs, Ei
 		circle(frame, cvPoint(imgpts[3].x, imgpts[3].y), myradius, CV_RGB(0, 0, 255), -1, 8, 0);
 		line(frame, cvPoint(imgpts[0].x, imgpts[0].y), cvPoint(imgpts[3].x, imgpts[3].y), CV_RGB(0, 0, 255), 4);
 		imshow("Axis", frame);
-
-		/*vectorVector2d laserpoints2d;
-		vectorVector3d laserpoints3d;
-		Eigen::Vector2d punkty2d(corners.at(7*6-1).x, corners.at(7 * 6 - 1).y);
-		laserpoints2d.push_back(punkty2d);
-
-
-		projectImagePointsOntoPlane(laserpoints2d, laserpoints3d, cameraMatrix, PlaneAtCheckerboard);
-		cout << "calculeted:" << laserpoints3d.at(0) << endl;*/
 
 		return true;
 	}
@@ -374,4 +306,92 @@ bool projectImagePointsOntoPlane(const vectorVector2d &pts,
 		}
 		return true;
 	}
+}
+
+bool camCalib()
+{
+	Mat temp;
+	Mat img[20];
+	string nazwa;
+	vector<vector<Point2f>> wek;
+	vector<Point3f> dobra;
+	vector<vector<Point3f>> dobra_wek;
+
+	namedWindow("image");
+	VideoCapture cap(0); // open the default camera
+	if (!cap.isOpened()) // check if we succeeded
+		return -1;
+
+	for (int i = 0; i < 8; i++)
+		for (int j = 0; j < 6; j++)
+		{
+			dobra.push_back(Point3f(j, i, 0));
+		}
+	while (1)
+	{
+		cap >> temp;
+		imshow("image", temp);
+		if (waitKey(30) >= 0) break;
+	}
+	
+	for (int i = 0; i < 20; )
+	{
+
+		temp.copyTo(img[i]);
+
+		Size patternsize(6, 8);
+		vector<Point2f> corners;
+
+		bool patternfound = findChessboardCorners(img[i], patternsize, corners,
+			CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
+			+ CALIB_CB_FAST_CHECK);
+
+		if (patternfound)
+		{
+			Mat subpixel_BW;
+			cvtColor(img[i], subpixel_BW, CV_BGR2GRAY);
+			cornerSubPix(subpixel_BW, corners, Size(11, 11), Size(-1, -1),
+				TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+
+			wek.push_back(corners);
+			dobra_wek.push_back(dobra);
+
+			namedWindow("Chessboard");
+			drawChessboardCorners(img[i], patternsize, Mat(corners), patternfound);
+			imshow("Chessboard", img[i]);
+			i++;
+		}
+
+		while (1)
+		{
+			cap >> temp;
+			imshow("image", temp);
+			if (waitKey(30) >= 0) break;
+		}
+
+	}
+
+
+	Mat cameraMatrix, distCoeffs;
+	vector <Mat>rv, tv;
+	cout << dobra_wek.size() << endl;
+	cout << wek.size() << endl;
+	calibrateCamera(dobra_wek, wek, img[0].size(), cameraMatrix, distCoeffs, rv, tv);
+
+
+	FileStorage file("cam_param.yml", cv::FileStorage::WRITE);
+	file << "cameraMatrix" << cameraMatrix << "distCoeffs" << distCoeffs;
+
+	file.release();
+
+	/*Mat tmp;
+	undistort(img[0], tmp, cameraMatrix, distCoeffs);
+	for (;;)
+	{
+		imshow("First Img undistorted", tmp);
+
+		if (waitKey(1) >= 0) break;
+
+	}*/
+	return 0;
 }
