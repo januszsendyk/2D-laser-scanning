cac2640a7f1be80adc4f65cb0aa41919ae3de0ec
Jarosław Mądry <jarasssss@gmail.com>
2018-05-30 16:42:35 +0200

    Basic Pylon features added.

--- LaserScan/main.cpp
+++ LaserScan/main.cpp
@@ -1,7 +1,11 @@
 #include <opencv2/opencv.hpp>
 #include <Eigen/Core>
 #include <Eigen/Dense>
-//#include <pcl/visualization/cloud_viewer.h>
+#include <pylon/PylonIncludes.h>
+#include <sstream>
+
+using namespace Pylon;
+using namespace GenApi;
 using namespace cv;
 using namespace std;
 
@@ -35,6 +39,7 @@ bool projectImagePointsOntoPlane(const vectorVector2d &pts,
 	const cv::Mat &cameraMatrix,
 	const Eigen::Vector4d &planeEq);
 bool camCalib();
+int PylonCapture(Mat& frame);
 //void simpleCloudVisualizer();
 
 
@@ -47,6 +52,13 @@ int main(int, char**)
 
 	namedWindow("frame");
 
+	while (1)
+	{
+		Mat pylon_frame;
+		PylonCapture(pylon_frame);
+		imshow("pylon_frame", pylon_frame);
+		waitKey(15);
+	}
 	/*createTrackbar("hsv_h_min", "Film", &hsv_h_min, hsv_h_min_max);
 	createTrackbar("hsv_s_min", "Film", &hsv_s_min, hsv_s_min_max);
 	createTrackbar("hsv_v_min", "Film", &hsv_v_min, hsv_v_min_max);
@@ -54,6 +66,7 @@ int main(int, char**)
 	createTrackbar("hsv_s_max", "Film", &hsv_s_max, hsv_s_max_max);
 	createTrackbar("hsv_v_max", "Film", &hsv_v_max, hsv_v_max_max);*/
 	//simpleCloudVisualizer();
+
 
 	FileStorage fs2("cam_param.yml", FileStorage::READ);
 
@@ -414,4 +427,104 @@ bool camCalib()
 
 	}*/
 	return 0;
+}
+int PylonCapture(Mat& frame)
+{
+
+	// Number of images to be grabbed.
+	static const uint32_t c_countOfImagesToGrab = 1;
+
+		// The exit code of the sample application.
+		int exitCode = 0;
+
+		// Automagically call PylonInitialize and PylonTerminate to ensure the pylon runtime system
+		// is initialized during the lifetime of this object.
+		Pylon::PylonAutoInitTerm autoInitTerm;
+
+		try
+		{
+			// Create an instant camera object with the camera device found first.
+			cout << "Creating Camera..." << endl;
+			CInstantCamera camera(CTlFactory::GetInstance().CreateFirstDevice());
+
+			// or use a device info object to use a specific camera
+			//CDeviceInfo info;
+			//info.SetSerialNumber("21694497");
+			//CInstantCamera camera( CTlFactory::GetInstance().CreateFirstDevice(info));
+			cout << "Camera Created." << endl;
+			// Print the model name of the camera.
+			cout << "Using device " << camera.GetDeviceInfo().GetModelName() << endl;
+
+			// The parameter MaxNumBuffer can be used to control the count of buffers
+			// allocated for grabbing. The default value of this parameter is 10.
+			camera.MaxNumBuffer = 10;
+
+			// create pylon image format converter and pylon image
+			CImageFormatConverter formatConverter;
+			formatConverter.OutputPixelFormat = PixelType_BGR8packed;
+			CPylonImage pylonImage;
+
+			// Create an OpenCV image
+			Mat openCvImage;
+
+			// Start the grabbing of c_countOfImagesToGrab images.
+			// The camera device is parameterized with a default configuration which
+			// sets up free-running continuous acquisition.
+			camera.StartGrabbing(c_countOfImagesToGrab);
+
+			// This smart pointer will receive the grab result data.
+			CGrabResultPtr ptrGrabResult;
+
+			// Camera.StopGrabbing() is called automatically by the RetrieveResult() method
+			// when c_countOfImagesToGrab images have been retrieved.
+			while (camera.IsGrabbing())
+			{
+				// Wait for an image and then retrieve it. A timeout of 5000 ms is used.
+				camera.RetrieveResult(5000, ptrGrabResult, TimeoutHandling_ThrowException);
+
+				// Image grabbed successfully?
+				if (ptrGrabResult->GrabSucceeded())
+				{
+					// Access the image data.
+					//cout << "SizeX: " << ptrGrabResult->GetWidth() << endl;
+					//cout << "SizeY: " << ptrGrabResult->GetHeight() << endl;
+					//const uint8_t *pImageBuffer = (uint8_t *)ptrGrabResult->GetBuffer();
+					//cout << "Gray value of first pixel: " << (uint32_t)pImageBuffer[0] << endl << endl;
+
+					// Convert the grabbed buffer to pylon imag
+					formatConverter.Convert(pylonImage, ptrGrabResult);
+					// Create an OpenCV image out of pylon image
+					openCvImage = cv::Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8UC3, (uint8_t *)pylonImage.GetBuffer());
+
+					openCvImage.copyTo(frame);
+					// Create a display window
+					//namedWindow("OpenCV Display Window", CV_WINDOW_NORMAL);//AUTOSIZE //FREERATIO
+																		   // Display the current image with opencv
+					//imshow("OpenCV Display Window", openCvImage);
+					// Define a timeout for customer's input in ms.
+					// '0' means indefinite, i.e. the next image will be displayed after closing the window 
+					// '1' means live stream
+					waitKey(1);
+
+				}
+				else
+				{
+					cout << "Error: " << ptrGrabResult->GetErrorCode() << " " << ptrGrabResult->GetErrorDescription() << endl;
+				}
+			}
+		}
+		catch (GenICam::GenericException &e)
+		{
+			// Error handling.
+			cerr << "An exception occurred." << endl
+				<< e.GetDescription() << endl;
+			exitCode = 1;
+		}
+
+		// Comment the following two lines to disable waiting on exit.
+		//cerr << endl << "Press Enter to exit." << endl;
+		//while (cin.get() != '\n');
+
+		return exitCode;
+
 }
