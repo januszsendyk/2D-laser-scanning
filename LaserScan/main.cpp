71b65650d5ab8b52e85389c94ba39616d03df50c
Jarosław Mądry <jarasssss@gmail.com>
2018-06-06 16:48:40 +0200

    Added Pylyon

--- LaserScan/main.cpp
+++ LaserScan/main.cpp
@@ -38,27 +38,42 @@ bool projectImagePointsOntoPlane(const vectorVector2d &pts,
 	vectorVector3d &pts3d,
 	const cv::Mat &cameraMatrix,
 	const Eigen::Vector4d &planeEq);
-bool camCalib();
-int PylonCapture(Mat& frame);
+bool camCalib(CInstantCamera& camera);
+void PylonConfig(CInstantCamera& camera);
+int PylonCapture(Mat& frame, CInstantCamera& camera);
 //void simpleCloudVisualizer();
 
 
 int main(int, char**)
 {
 	Eigen::Vector4d PlaneEquation;
-	VideoCapture cap(0); // open the default camera
-	if (!cap.isOpened()) // check if we succeeded
-		return -1;
+	//VideoCapture cap(0); // open the default camera
+	//if (!cap.isOpened()) // check if we succeeded
+		//return -1;
 
 	namedWindow("frame");
 
-	while (1)
+
+	Mat pylon_frame;
+	// Create an instant camera object with the camera device found first.
+	Pylon::PylonAutoInitTerm autoInitTerm;
+	try
 	{
-		Mat pylon_frame;
-		PylonCapture(pylon_frame);
-		imshow("pylon_frame", pylon_frame);
-		waitKey(15);
-	}
+		cout << "Creating Camera..." << endl;
+		//IPylonDevice* pDevice = CTlFactory::GetInstance().CreateFirstDevice();
+		CInstantCamera camera(CTlFactory::GetInstance().CreateFirstDevice());
+		cout << "Camera Created." << endl;
+		// Print the model name of the camera.
+		cout << "Using device " << camera.GetDeviceInfo().GetModelName() << endl;
+		//PylonConfig(camera);
+		/*while (1)
+		{
+			PylonCapture(pylon_frame, camera);
+			//PylonCapture(pylon_frame);
+			imshow("pylon_frame", pylon_frame);
+			waitKey(15);
+		}*/
+		
 	/*createTrackbar("hsv_h_min", "Film", &hsv_h_min, hsv_h_min_max);
 	createTrackbar("hsv_s_min", "Film", &hsv_s_min, hsv_s_min_max);
 	createTrackbar("hsv_v_min", "Film", &hsv_v_min, hsv_v_min_max);
@@ -102,8 +117,9 @@ int main(int, char**)
 				Mat frame;
 				while (1)
 				{
-					if (!cap.read(frame))
-						cap >> frame;
+					//if (!cap.read(frame))
+						//cap >> frame;
+					PylonCapture(frame, camera);
 					imshow("frame", frame);
 					Mat tresholded;
 					inRange(frame, Scalar(hsv_h_min, hsv_s_min, hsv_v_min), Scalar(hsv_h_max, hsv_s_max, hsv_v_max), tresholded);
@@ -142,11 +158,20 @@ int main(int, char**)
 	}
 	else
 	{
-		camCalib();
+		camCalib(camera);
 	}
 
 	waitKey(0);
 	return 0;
+
+	}
+	catch (GenICam::GenericException &e)
+	{
+		// Error handling.
+		cerr << "An exception occurred." << endl
+			<< e.GetDescription() << endl;
+		//exitCode = 1;
+	}
 }
 
 //void simpleCloudVisualizer()
@@ -341,7 +366,7 @@ bool projectImagePointsOntoPlane(const vectorVector2d &pts,
 	}
 }
 
-bool camCalib()
+bool camCalib(CInstantCamera& camera)
 {
 	Mat temp;
 	Mat img[20];
@@ -351,18 +376,22 @@ bool camCalib()
 	vector<vector<Point3f>> dobra_wek;
 
 	namedWindow("image");
-	VideoCapture cap(0); // open the default camera
-	if (!cap.isOpened()) // check if we succeeded
-		return -1;
+	//VideoCapture cap(0); // open the default camera
+	//if (!cap.isOpened()) // check if we succeeded
+		//return -1;
 
-	for (int i = 0; i < 8; i++)
-		for (int j = 0; j < 6; j++)
+	for (int i = 0; i < 6; i++)
+		for (int j = 0; j < 7; j++)
 		{
 			dobra.push_back(Point3f(j, i, 0));
 		}
 	while (1)
 	{
-		cap >> temp;
+		Mat test;
+
+		PylonCapture(temp, camera);
+		//test.copyTo(temp);
+		//cap >> temp;
 		imshow("image", temp);
 		if (waitKey(30) >= 0) break;
 	}
@@ -372,9 +401,10 @@ bool camCalib()
 
 		temp.copyTo(img[i]);
 
-		Size patternsize(6, 8);
+		Size patternsize(7, 6);
 		vector<Point2f> corners;
 
+		imshow("Chessboard", img[i]);
 		bool patternfound = findChessboardCorners(img[i], patternsize, corners,
 			CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
 			+ CALIB_CB_FAST_CHECK);
@@ -393,11 +423,15 @@ bool camCalib()
 			drawChessboardCorners(img[i], patternsize, Mat(corners), patternfound);
 			imshow("Chessboard", img[i]);
 			i++;
+			cout<< i << " of 20" <<endl;
 		}
 
 		while (1)
 		{
-			cap >> temp;
+			//Mat test;
+
+			PylonCapture(temp, camera);
+			//test.copyTo(temp);
 			imshow("image", temp);
 			if (waitKey(30) >= 0) break;
 		}
@@ -428,53 +462,33 @@ bool camCalib()
 	}*/
 	return 0;
 }
-int PylonCapture(Mat& frame)
+
+int PylonCapture(Mat& frame, CInstantCamera& camera)// IPylonDevice* pDevice)
 {
 
-	// Number of images to be grabbed.
-	static const uint32_t c_countOfImagesToGrab = 1;
+	int exitCode = 0;
+	try
+	{
+		// The parameter MaxNumBuffer can be used to control the count of buffers
+		// allocated for grabbing. The default value of this parameter is 10.
+		camera.MaxNumBuffer = 10;
+		
+		// create pylon image format converter and pylon image
+		CImageFormatConverter formatConverter;
+		formatConverter.OutputPixelFormat = PixelType_BGR8packed;
+		CPylonImage pylonImage;
 
-		// The exit code of the sample application.
-		int exitCode = 0;
+		// Create an OpenCV image
+		Mat openCvImage;
 
-		// Automagically call PylonInitialize and PylonTerminate to ensure the pylon runtime system
-		// is initialized during the lifetime of this object.
-		Pylon::PylonAutoInitTerm autoInitTerm;
+		// Start the grabbing of c_countOfImagesToGrab images.
+		// The camera device is parameterized with a default configuration which
+		// sets up free-running continuous acquisition.
+		static const uint32_t c_countOfImagesToGrab = 1;
+		camera.StartGrabbing(c_countOfImagesToGrab);
 
-		try
-		{
-			// Create an instant camera object with the camera device found first.
-			cout << "Creating Camera..." << endl;
-			CInstantCamera camera(CTlFactory::GetInstance().CreateFirstDevice());
-
-			// or use a device info object to use a specific camera
-			//CDeviceInfo info;
-			//info.SetSerialNumber("21694497");
-			//CInstantCamera camera( CTlFactory::GetInstance().CreateFirstDevice(info));
-			cout << "Camera Created." << endl;
-			// Print the model name of the camera.
-			cout << "Using device " << camera.GetDeviceInfo().GetModelName() << endl;
-
-			// The parameter MaxNumBuffer can be used to control the count of buffers
-			// allocated for grabbing. The default value of this parameter is 10.
-			camera.MaxNumBuffer = 10;
-
-			// create pylon image format converter and pylon image
-			CImageFormatConverter formatConverter;
-			formatConverter.OutputPixelFormat = PixelType_BGR8packed;
-			CPylonImage pylonImage;
-
-			// Create an OpenCV image
-			Mat openCvImage;
-
-			// Start the grabbing of c_countOfImagesToGrab images.
-			// The camera device is parameterized with a default configuration which
-			// sets up free-running continuous acquisition.
-			camera.StartGrabbing(c_countOfImagesToGrab);
-
-			// This smart pointer will receive the grab result data.
-			CGrabResultPtr ptrGrabResult;
-
+		// This smart pointer will receive the grab result data.
+		CGrabResultPtr ptrGrabResult;
 			// Camera.StopGrabbing() is called automatically by the RetrieveResult() method
 			// when c_countOfImagesToGrab images have been retrieved.
 			while (camera.IsGrabbing())
@@ -485,27 +499,18 @@ int PylonCapture(Mat& frame)
 				// Image grabbed successfully?
 				if (ptrGrabResult->GrabSucceeded())
 				{
-					// Access the image data.
-					//cout << "SizeX: " << ptrGrabResult->GetWidth() << endl;
-					//cout << "SizeY: " << ptrGrabResult->GetHeight() << endl;
-					//const uint8_t *pImageBuffer = (uint8_t *)ptrGrabResult->GetBuffer();
-					//cout << "Gray value of first pixel: " << (uint32_t)pImageBuffer[0] << endl << endl;
-
+					
 					// Convert the grabbed buffer to pylon imag
 					formatConverter.Convert(pylonImage, ptrGrabResult);
 					// Create an OpenCV image out of pylon image
 					openCvImage = cv::Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8UC3, (uint8_t *)pylonImage.GetBuffer());
 
 					openCvImage.copyTo(frame);
-					// Create a display window
-					//namedWindow("OpenCV Display Window", CV_WINDOW_NORMAL);//AUTOSIZE //FREERATIO
-																		   // Display the current image with opencv
-					//imshow("OpenCV Display Window", openCvImage);
+
 					// Define a timeout for customer's input in ms.
 					// '0' means indefinite, i.e. the next image will be displayed after closing the window 
 					// '1' means live stream
-					waitKey(1);
-
+					//waitKey(1);
 				}
 				else
 				{
