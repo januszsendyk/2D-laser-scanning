23754449ef9e89a04173b87bc14e7b6c0635d4cc
Jarosław Mądry <jarasssss@gmail.com>
2018-06-14 15:54:34 +0200

    improved calibrations

--- LaserScan/display.m
+++ LaserScan/display.m
@@ -1,6 +1,17 @@
-data=importdata('cloud.txt');
+data=importdata('cloud_0.txt');
 data=data.*17 %mm
 figure (1)
 scatter3(data(1,2:end),data(2,2:end),data(3,2:end), 1);
 figure (2)
 plot(data(1,2:end),data(3,2:end),'.')
+
+data=importdata('cloud_1.txt');
+data=data.*17 %mm
+figure (1)
+hold on;
+scatter3(data(1,2:end),data(2,2:end),data(3,2:end), 1);
+hold off;
+figure (2)
+hold on;
+plot(data(1,2:end),data(3,2:end),'r.')
+hold off;
